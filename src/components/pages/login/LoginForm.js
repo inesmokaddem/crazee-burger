bb14c6b0dca7cbcc43a471834ca236bd500ee160
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-15 15:28:01 +0100

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -24,7 +24,7 @@ export default function LoginForm() {
         <div>
             <LoginFormStyled action="submit" onSubmit={handleSubmit}>
                 <h1>Bienvenue chez nous !</h1>
-                <br/>
+                <hr/>
                 <h2>Connectez-vous</h2>
                 <div>
                     <input 
@@ -41,5 +41,27 @@ export default function LoginForm() {
 }
 
 const LoginFormStyled = styled.div`
-    background: blue;
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    margin: 0 auto;
+    padding: 2.5rem 2rem;
+    border-radius: 5px;
+    font-family: "Amatic SC", cursive;
+
+    hr {
+        border: 1.5px solid #f56a2c;
+        margin-bottom: 40px;
+    }
+
+    h1 {
+        color: white;
+        font-size: 48px;
+    }
+    h2 {
+        color: #8e8b8b;
+        margin: 20px 10px 10px;
+        color: white;
+        font-size: 36px;
+    }
 `;
