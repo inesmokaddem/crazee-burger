88224b57a0e71906fb9c8119d804b2dd463336cb
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-15 16:06:42 +0100

    💄 F03: LOGIN > LoginForm > Btn Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,6 +2,7 @@ import { React, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { BsPersonCircle } from "react-icons/bs"
+import { IoChevronForward } from "react-icons/io5"
 import { theme } from '../../../theme'
 
 export default function LoginForm() {
@@ -38,7 +39,10 @@ export default function LoginForm() {
                             required 
                         />
                     </div>
-                    <button>Accéder à mon espace</button>
+                    <button className='button-with-icon'>
+                        <span>Accéder à mon espace</span>
+                        <IoChevronForward className='icon'/>
+                    </button>
                 </div>
             </LoginFormStyled>
         </div>
@@ -89,12 +93,57 @@ const LoginFormStyled = styled.div`
             font-size: 15px;
             color: #17161a;
             width: 100%;
-            display: flex;
+            /* display: flex; */
         }
 
         &::placeholder {
             background: white;
             color: lightgrey;
+        }
+    }
+    .button-with-icon {
+        width: 100%;
+        border: 1px solid red;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        white-space: nowrap;
+        text-decoration: none;
+        line-height: 1;
+
+        padding: 18px 24px;
+        border-radius: 5px;
+        font-size: 15px;
+        font-weight: 800;
+        color: white;
+        background-color: #ff9f1b;
+        border: 1px solid #ff9f1b;
+
+        &:hover:not(:disabled) {
+            background-color: white;
+            color: #ff9f1b;
+            border: 1px solid #ff9f1b;
+            transition: all 200ms ease-out;
+        }
+
+        &:active {
+            color: white;
+            background-color: #ff9f1b;
+            border: 1px solid #ff9f1b;
+        }
+
+        &:disabled {
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
+
+        .icon {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 15px;
+            margin-left: 10px;
         }
     }
 `;
