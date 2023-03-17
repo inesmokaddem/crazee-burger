b890ab9d97400c09c54edcbffb5aa1615c958d32
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-17 18:06:36 +0100

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 import NavbarRightSide from "./NavbarRightSide";
 import Logo from "../../reusable-ui/Logo"
 import { theme } from "../../../theme";
+import { refreshPage } from "../../../utils/window";
+
 export default function Navbar({username}) {
+
   return (
     <NavbarStyled>
-        <Logo/>
+        <Logo className="logo-order-page" onClick={refreshPage}/>
         <NavbarRightSide username={username}/>
     </NavbarStyled>
   )
@@ -13,11 +16,15 @@ export default function Navbar({username}) {
 
 const NavbarStyled = styled.nav`
   background: ${theme.colors.white};
-    height: 10vh;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 20px;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
 
-    border-top-left-radius: ${theme.borderRadius.extraRound};
-    border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+
+  .logo-order-page {
+    cursor: pointer;
+  }
 `;
