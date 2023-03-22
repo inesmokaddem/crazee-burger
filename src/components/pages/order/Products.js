084e27189946a38974bf3c284e0629242d953102
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-22 21:12:31 +0100

    ♻️ F05: ORDER > Menu > moved products css in Products.js

--- src/components/pages/order/Products.js
+++ src/components/pages/order/Products.js
@@ -1,8 +1,10 @@
+import styled from "styled-components";
+import { theme } from "../../../theme";
 import PrimaryButton from "../../reusable-ui/PrimaryButton";
 
 export default function Products({title, imageSource, price}) {
   return (
-    <div className='product'>
+    <ProductsStyled>
         <div className='product__img'>
             <img src={imageSource} alt={title}></img>
         </div>
@@ -13,6 +15,47 @@ export default function Products({title, imageSource, price}) {
                 <PrimaryButton className="product__btn" label={"Ajouter"} />
             </div>
         </div>
-    </div>
+    </ProductsStyled>
   )
 }
+
+const ProductsStyled = styled.div`
+    border-radius: 15px;
+    width: 240px;
+    height: 330px;
+    box-shadow: -8px 8px 20px rgba(0, 0, 0, 0.2);
+
+    .product__img {
+        width: 200px;
+        height: 145px;
+        margin: 0 auto;
+
+        img {
+            max-width: 100%;
+            height: auto;
+            object-fit: cover;
+        }
+    }
+
+    .product__title{
+        font-size: ${theme.fonts.size.P4};
+        font-family: "Amatic SC", cursive;
+        font-weight: ${theme.fonts.weights.bold};
+        line-height: 45px;
+        margin-left: 25px;
+        text-overflow: ellipsis;
+        overflow: hidden; 
+        width: 185px; 
+        white-space: nowrap;
+    }
+    .product__infos{
+        display: flex;
+        justify-content: space-around;
+        .product__price {
+            color: ${theme.colors.primary};
+        }
+        .product__btn {
+            padding: 16px 24px;
+        }
+    }
+`;
