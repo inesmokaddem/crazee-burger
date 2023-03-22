fe94ade5e2d23a3537203cc2e4371ba63028d029
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-22 19:32:54 +0100

    💄 F05: ORDER > Menu > add styling and elements for  products cards

--- src/components/pages/order/MenuCard.js
+++ src/components/pages/order/MenuCard.js
@@ -1,27 +1,36 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import { fakeMenu2 } from '../../../data/fakeMenu';
+import { theme } from '../../../theme';
 import PrimaryButton from '../../reusable-ui/PrimaryButton';
 
-export default function MenuCard() {
+export default function MenuCard(product) {
 
     const [products, setProducts] = useState(fakeMenu2);
 
   return (
     <MenuCardStyled>
         {products.map((product) => {
-            return <div className='product'>
-                        <p>{product.title}</p>
-                        <p>{product.price}</p>
-
+            return (
+            <div className='product'>
+                <div className='product__img'>
+                    <img src={product.imageSource} alt={product.title}></img>
+                </div>
+                <div className='product__description'>
+                    <p className='product__title'>{product.title}</p>
+                    <div className='product__infos'>
+                        <p className='product__price'>{product.price} €</p>
+                        <PrimaryButton label={"Ajouter"} />
                     </div>
+                </div>
+            </div>
+            )
         })}
     </MenuCardStyled>
   )
 }
 
 const MenuCardStyled = styled.div`
-    border: 1px solid green;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-row-gap: 60px;
@@ -29,8 +38,38 @@ const MenuCardStyled = styled.div`
     justify-items: center;
 
     .product {
-        border: 1px solid purple;
+        border-radius: 15px;
         width: 240px;
         height: 330px;
+        box-shadow: -8px 8px 20px rgba(0, 0, 0, 0.2);
+
+        .product__img {
+            width: 200px;
+            height: 145px;
+            margin: 0 auto;
+            img {
+                width: 100%;
+                height: auto;
+            }
+        }
+
+        .product__title{
+            font-size: ${theme.fonts.size.P4};
+            font-family: "Amatic SC", cursive;
+            font-weight: ${theme.fonts.weights.bold};
+            line-height: 45px;
+            margin-left: 25px;
+            /* identical to box height */
+
+            display: flex;
+            align-items: center;
+        }
+        .product__infos{
+            display: flex;
+            justify-content: space-around;
+            .product__price {
+                color: ${theme.colors.primary};
+            }
+        }
     }
 `;
