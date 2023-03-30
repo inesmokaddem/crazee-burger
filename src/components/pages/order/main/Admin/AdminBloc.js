7b2470105e2fc772fcdae2903d7c5a9d04d49709
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-03-30 16:03:51 +0200

    ♻️ F07: ADMIN BLOC > Create AdminTabs component and styling

--- src/components/pages/order/main/Admin/AdminBloc.js
+++ src/components/pages/order/main/Admin/AdminBloc.js
@@ -1,23 +1,11 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
-import {AiOutlinePlus} from "react-icons/ai"
-import {MdModeEditOutline} from "react-icons/md"
-import {FiChevronDown} from "react-icons/fi"
+import AdminTabs from "./AdminTabs";
 
 export default function AdminBloc() {
   return (
     <AdminBlocStyled>
-      <div className="admin-tabs">
-        <div className="tab">
-        <button className="toggle-button"><FiChevronDown/></button>
-        </div>
-        <div className="tab">
-          <AiOutlinePlus/> Ajouter un produit
-        </div>
-        <div className="tab">
-          <MdModeEditOutline/> Modifier un produit
-        </div>
-      </div>
+      <AdminTabs/>
       <div className="admin-panel">
         Ajouter un produit
       </div>
@@ -30,33 +18,6 @@ const AdminBlocStyled = styled.div`
     bottom: 0;
     right: 0;
     left: 0;
-    .admin-tabs {
-      display: flex;
-      color: #93A2B1;
-      margin-left: 70px;
-
-      .tab {
-        display: flex;
-        align-items: center;
-        padding: 10px 20px;
-        gap: 13.02px;
-
-        background: #FFFFFF;
-        border-width: 1px 1px 2px 1px;
-        border-style: solid;
-        border-color: #E4E5E9;
-        box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
-        border-radius: 5px 5px 0px 0px;
-
-        cursor: pointer;
-
-        .toggle-button{
-          background: white;
-          color: #93A2B1;
-          border: none;
-        }
-      }
-    }
     .admin-panel {
       height: 250px;
       background: ${theme.colors.white};
---
+++ src/components/pages/order/main/Admin/AdminTabs.js
@@ -0,0 +1,49 @@
+import {AiOutlinePlus} from "react-icons/ai"
+import {MdModeEditOutline} from "react-icons/md"
+import {FiChevronDown} from "react-icons/fi"
+import styled from "styled-components";
+import {theme} from "../../../../../theme"
+
+export default function AdminTabs() {
+  return (
+    <AdminTabsStyled>
+        <div className="tab">
+            <button className="toggle-button"><FiChevronDown/></button>
+        </div>
+        <div className="tab">
+            <AiOutlinePlus/> Ajouter un produit
+        </div>
+        <div className="tab">
+            <MdModeEditOutline/> Modifier un produit
+        </div>
+    </AdminTabsStyled>
+  )
+}
+
+const AdminTabsStyled = styled.div`
+    display: flex;
+    color: ${theme.colors.greySemiDark};
+    margin-left: 70px;
+
+    .tab {
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    gap: 13.02px;
+
+    background: ${theme.colors.white};
+    border-width: 1px 1px 2px 1px;
+    border-style: solid;
+    border-color: ${theme.colors.greyLight};
+    box-shadow: ${theme.shadows.subtle};
+    border-radius: ${theme.borderRadius.round};
+
+    cursor: pointer;
+
+    .toggle-button{
+        background: ${theme.colors.white};
+        color: ${theme.colors.greySemiDark};
+        border: none;
+    }
+    }
+`;
