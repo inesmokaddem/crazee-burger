dd1e1e19b68f9b67f2296c1c4e6a9b2b927d5153
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-05-26 00:35:09 +0200

    💄 F08:ADMIN > AddProductFom > styling message submit and button

--- src/components/pages/order/main/Admin/AdminPanel/AddProductForm.js
+++ src/components/pages/order/main/Admin/AdminPanel/AddProductForm.js
@@ -88,9 +88,9 @@ export default function AddProductForm() {
         />
         {isSubmitted && (
           <div className="submit-message">
-            <span>Ajouté avec succès</span>
-            <FiCheck className="icon" />
-          </div>
+          <FiCheck className="icon" />
+          <span className="message">Ajouté avec succès !</span>
+        </div>
         )}
       </div>
     </AddProductFormStyled>
@@ -143,11 +143,35 @@ const AddProductFormStyled = styled.form`
   .submit {
     grid-area: 4 / 2 / -1 / -1;
     display: flex;
+    align-items: center;
+    position: relative;
+    top: 3px;
 
+    .submit-button {
+      /* height: 100%; */
+    }
 
     .submit-message {
+      /* border: 1px solid blue; */
       display: flex;
+      justify-content: center;
       align-items: center;
+      margin-left: 5px;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left: 10px;
+        width: 1em;
+        height: 1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+      .message {
+        margin-left: 5px;
+        font-size: ${theme.fonts.size.SM};
+        color: ${theme.colors.success};
+      }
     }
-  }
+  } 
 `
