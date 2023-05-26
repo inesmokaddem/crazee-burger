889c4af0673e80aea6a7ad92c1dd062a843a9c19
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-05-26 17:31:28 +0200

    ♻️ F08: Extract ImagePreview

--- src/components/pages/order/main/Admin/AdminPanel/AddProductForm.js
+++ src/components/pages/order/main/Admin/AdminPanel/AddProductForm.js
@@ -8,6 +8,7 @@ import { BsFillCameraFill } from "react-icons/bs"
 import { MdOutlineEuro } from "react-icons/md"
 import { theme } from "../../../../../../theme"
 import Button from "../../../../../reusable-ui/Button"
+import ImagePreview from "./ImagePreview"
 
 export const EMPTY_PRODUCT = {
   id: "",
@@ -42,13 +43,7 @@ export default function AddProductForm() {
 
   return (
     <AddProductFormStyled onSubmit={handleSubmit}>
-      <div className="image-preview">
-        {newProduct.imageSource ? (
-          <img src={newProduct.imageSource} alt={newProduct.title} />
-        ) : (
-          <div className="empty-image">Aucune image</div>
-        )}
-      </div>
+      <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
       <div className="input-fields">
         <TextInput
           name="title"
@@ -105,33 +100,6 @@ const AddProductFormStyled = styled.form`
   grid-template-rows: repeat(4, 1fr);
   grid-column-gap: 20px;
   grid-row-gap: 8px;
-
-  .image-preview {
-    grid-area: 1 / 1 / 4 / 2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .empty-image {
-      /* background-color: green; */
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: 1px solid ${theme.colors.greyLight};
-      line-height: 1.5;
-      color: ${theme.colors.greySemiDark};
-      border-radius: ${theme.borderRadius.round};
-    }
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
 
   .input-fields {
     grid-area: 1 / 2 / -2 / 3;
