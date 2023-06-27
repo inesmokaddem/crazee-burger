16e72a4b9507aac51520595c89efe7cd2dd3ba5b
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-06-27 15:29:01 +0200

    ♻️ F10: BASKET > Create useDisplaySuccessMessage.js using custom hooks

---
+++ src/hooks/useSuccessMessage.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export const useSuccessMessage = (timeDelay = 2000) => {
+
+    // state
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    // comportements
+    const displaySuccessMessage = () => {
+        setIsSubmitted(true)
+        setTimeout(() => {
+          setIsSubmitted(false)
+        }, timeDelay)
+        // setNewProduct(EMPTY_PRODUCT)
+      }
+
+      return {isSubmitted, displaySuccessMessage}
+  }
