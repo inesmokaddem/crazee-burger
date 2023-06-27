5214f995afb79b40baab8302494e437fc66b83d5
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-06-27 11:12:07 +0200

    🚧 F10: BASKET > Create struture and styling for Basket

--- src/components/pages/order/main/Basket/Basket.js
+++ src/components/pages/order/main/Basket/Basket.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
+import { theme } from '../../../../../theme';
 
 export default function Basket() {
   return (
-    <BasketStyled>Basket</BasketStyled>
+    <BasketStyled>
+      <div className='head'>head</div>
+      <div className='body'>body</div>
+      <div className='footer'>footer</div>
+
+    </BasketStyled>
   )
 }
 
 const BasketStyled = styled.div`
   background-color: pink;
+  display: flex;
+  flex-direction: column;
+
+  .head {
+    height: 70px;
+    background-color: ${theme.colors.background_dark};
+  }
+
+  .body {
+    flex: 1;
+  }
+
+  .footer {
+    background-color: ${theme.colors.background_dark};
+    height: 70px;
+
+  }
 `;
